d2bf2dbd52dfc206da6bd578d385761ac942ff48
SeongJun Ha <agopwns@gmail.com>
2024-12-01 17:40:39 +0900

    반응형 레이아웃 만들기 & 네비게이션 바 고정하기

--- src/app/page.js
+++ src/app/page.js
@@ -11,9 +11,11 @@ import { Navigation } from "@/components/Navigation";
 
 export default function Home() {
   return (
-    <main className="flex flex-col gap-8 row-start-2 items-center w-full lg:max-w-3xl mx-auto pt-8">
-      <Navigation />
-      <div className="bg-red-500 w-full h-96">body</div>
+    <main className="flex flex-col gap-2 lg:gap-8 row-start-2 items-center w-full lg:max-w-3xl mx-auto lg:pt-8 px-2 pt-2">
+      <div className="sticky top-0 z-50 w-full bg-background">
+        <Navigation />
+      </div>
+      <div className="bg-red-500 w-full h-[2000px]">body</div>
     </main>
   );
 }
